81c245968ea77bbb70a01a4ca8495cdc2cdcbc17
Roman Barmin <roma.barmin.06@gmail.com>
2025-12-23 11:01:48 +0300

    Создание записей в БД через форму

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from datetime import date
 
 from .forms import BirthdayForm
+from .models import Birthday
 
 
 def calculate_birthday_countdown(birthday):
@@ -39,8 +40,14 @@ def birthday(request):
     form = BirthdayForm(request.POST or None)
     context = {'form': form}
     if form.is_valid():
+        form.save()
         birthday_countdown = calculate_birthday_countdown(
             form.cleaned_data['birthday']
         )
         context.update({'birthday_countdown': birthday_countdown})
     return render(request, 'birthday/birthday.html', context)
+
+def birthday_list(request):
+    birthdays = Birthday.objects.all()
+    context = {'birthdays': birthdays}
+    return render(request, 'birthday/birthday_list.html', context)
