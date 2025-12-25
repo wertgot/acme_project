33b7ed0ff5045f26bfeeda08966f4c12656a45b4
Roman Barmin <roma.barmin.06@gmail.com>
2025-12-25 10:25:36 +0300

    Отправка писем. Эмуляция почтового сервера

--- acme_project/acme_project/settings.py
+++ acme_project/acme_project/settings.py
@@ -95,3 +95,7 @@ INTERNAL_IPS = [
 ]
 
 MEDIA_ROOT = BASE_DIR / 'media'
+
+EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
+
+EMAIL_FILE_PATH = BASE_DIR / 'sent_emails'
--- acme_project/birthday/forms.py
+++ acme_project/birthday/forms.py
@@ -4,6 +4,8 @@ from .models import Birthday
 
 from django.core.exceptions import ValidationError
 
+from django.core.mail import send_mail
+
 
 
 BEATLES = {'Джон Леннон', 'Пол Маккартни', 'Джордж Харрисон', 'Ринго Старр'}
@@ -21,11 +23,17 @@ class BirthdayForm(forms.ModelForm):
         return first_name.split()[0]
 
     def clean(self):
-        # Вызов родительского метода clean.
         super().clean()
         first_name = self.cleaned_data['first_name']
         last_name = self.cleaned_data['last_name']
         if f'{first_name} {last_name}' in BEATLES:
+            send_mail(
+                subject='Another Beatles member',
+                message=f'{first_name} {last_name} пытался опубликовать запись!',
+                from_email='birthday_form@acme.not',
+                recipient_list=['admin@acme.not'],
+                fail_silently=True,
+            )
             raise ValidationError(
                 'Мы тоже любим Битлз, но введите, пожалуйста, настоящее имя!'
             )
