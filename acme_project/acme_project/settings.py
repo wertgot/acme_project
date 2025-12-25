7f1b49cc40e4e52a9e77cc16d13324f1d1528bd3
Roman Barmin <roma.barmin.06@gmail.com>
2025-12-25 15:03:25 +0300

    Настройка страниц смены и восстановления пароля

--- acme_project/acme_project/settings.py
+++ acme_project/acme_project/settings.py
@@ -99,3 +99,7 @@ MEDIA_ROOT = BASE_DIR / 'media'
 EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
 
 EMAIL_FILE_PATH = BASE_DIR / 'sent_emails'
+
+LOGIN_REDIRECT_URL = 'pages:homepage'
+
+LOGIN_URL = 'login'
